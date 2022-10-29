b0e5ac401e12f421af16171eb0422109eb102b08
paronson <paronson@qw-corp.com>
2022-10-29 08:23:36 -0500

    grpc status active on server/index.js

--- server/index.js
+++ server/index.js
@@ -7,11 +7,39 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
+const LndGrpc = require("lnd-grpc");
+
 /**
  * Express route handlers
  */
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/api/info", async () => {
+    /**
+     * GET /api/info
+     */
+    console.log("BEGIN getInfo()");
+    const grpc = new LndGrpc({
+        lndconnectUri:
+            "lndconnect://127.0.0.1:10001?cert=MIICJjCCAc2gAwIBAgIRAOltkfkMF2l6F_YYoQ_SnTIwCgYIKoZIzj0EAwIwMTEfMB0GA1UEChMWbG5kIGF1dG9nZW5lcmF0ZWQgY2VydDEOMAwGA1UEAxMFYWxpY2UwHhcNMjIwODExMjEwNTU3WhcNMjMxMDA2MjEwNTU3WjAxMR8wHQYDVQQKExZsbmQgYXV0b2dlbmVyYXRlZCBjZXJ0MQ4wDAYDVQQDEwVhbGljZTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABOwkAz0Js8ZHnyxX5NYbGrp-PuWyoXOz98XHNS16l_8hr3Mb7yjN0j3LReKZj9zUR6nUaj0c-IU5GIGc3x3KpgGjgcUwgcIwDgYDVR0PAQH_BAQDAgKkMBMGA1UdJQQMMAoGCCsGAQUFBwMBMA8GA1UdEwEB_wQFMAMBAf8wHQYDVR0OBBYEFJS5FSPqtw-oJZybttUM-pFXFfUiMGsGA1UdEQRkMGKCBWFsaWNlgglsb2NhbGhvc3SCBWFsaWNlgg5wb2xhci1uNC1hbGljZYIEdW5peIIKdW5peHBhY2tldIIHYnVmY29ubocEfwAAAYcQAAAAAAAAAAAAAAAAAAAAAYcErBQAAjAKBggqhkjOPQQDAgNHADBEAiBycJKvte_IvaTbhahwa65Ux294bwtNAmHSRLV8Bnj93gIgBulGD39ZwK4L-0s8evyG1d7su542Thdh7er5kCgpZoc&macaroon=AgEDbG5kAvgBAwoQCIbSxfenIg62ObaXYAPFehIBMBoWCgdhZGRyZXNzEgRyZWFkEgV3cml0ZRoTCgRpbmZvEgRyZWFkEgV3cml0ZRoXCghpbnZvaWNlcxIEcmVhZBIFd3JpdGUaIQoIbWFjYXJvb24SCGdlbmVyYXRlEgRyZWFkEgV3cml0ZRoWCgdtZXNzYWdlEgRyZWFkEgV3cml0ZRoXCghvZmZjaGFpbhIEcmVhZBIFd3JpdGUaFgoHb25jaGFpbhIEcmVhZBIFd3JpdGUaFAoFcGVlcnMSBHJlYWQSBXdyaXRlGhgKBnNpZ25lchIIZ2VuZXJhdGUSBHJlYWQAAAYgayjXw6gX012qd_nMc2BXVkqNxTBv2mORACvJnwX7bNo",
+    });
+
+    await grpc.connect();
+
+    console.log(grpc.state);
+
+    // const { token } = req.body;
+    // if (!token) throw new Error('Your node is not connected!');
+    // // find the node that's making the request
+    // const node = db.getNodeByToken(token);
+    // if (!node) throw new Error('Node not found with this token');
+    // // get the node's pubkey and alias
+    // const rpc = nodeManager.getRpc(node.token);
+    // const { alias, identityPubkey: pubkey } = await rpc.getInfo();
+    // const { balance } = await rpc.channelBalance();
+    // res.send({ alias, balance, pubkey });
 });
 
 app.listen(4000, (err) => {
