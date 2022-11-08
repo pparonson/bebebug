e6c9d6184b7a031c54969c264921626aacd727fe
paronson <paronson@qw-corp.com>
2022-11-08 11:54:27 -0600

    temporarily move routes to handler

--- server/index.js
+++ server/index.js
@@ -15,118 +15,118 @@ const handler = new Handler(app);
 /**
  * Axios handlers
  */
-const sendGetRequest = async (url, route = "/") => {
-    try {
-        const res = await fetch(`${url}${route}`, {
-            signal: AbortSignal.timeout(5000),
-        });
-        if (res.ok) {
-            const data = await res.json();
-            console.log(`data: ${data}`);
-            return data;
-        } else {
-            console.log("Fetch failed to return a response");
-            return {
-                status: 500,
-                message: "Fetch failed to return a response",
-            };
-        }
-    } catch (error) {
-        if (error.name === "TimeoutError") {
-            console.error(`Error: TimeoutError - ${config.defaultTimeout} ms`);
-        } else if (error.name === "AbortError") {
-            console.error(
-                "Fetch aborted by user action (browser stop button, closing tab, etc."
-            );
-        } else if (error.name === "TypeError") {
-            console.error("AbortSignal.timeout() method is not supported");
-        } else {
-            // A network error, or some other problem.
-            console.error(
-                `Error: type: ${error.name}, message: ${error.message}`
-            );
-        }
-    }
-};
+// const sendGetRequest = async (url, route = "/") => {
+//     try {
+//         const res = await fetch(`${url}${route}`, {
+//             signal: AbortSignal.timeout(5000),
+//         });
+//         if (res.ok) {
+//             const data = await res.json();
+//             console.log(`data: ${data}`);
+//             return data;
+//         } else {
+//             console.log("Fetch failed to return a response");
+//             return {
+//                 status: 500,
+//                 message: "Fetch failed to return a response",
+//             };
+//         }
+//     } catch (error) {
+//         if (error.name === "TimeoutError") {
+//             console.error(`Error: TimeoutError - ${config.defaultTimeout} ms`);
+//         } else if (error.name === "AbortError") {
+//             console.error(
+//                 "Fetch aborted by user action (browser stop button, closing tab, etc."
+//             );
+//         } else if (error.name === "TypeError") {
+//             console.error("AbortSignal.timeout() method is not supported");
+//         } else {
+//             // A network error, or some other problem.
+//             console.error(
+//                 `Error: type: ${error.name}, message: ${error.message}`
+//             );
+//         }
+//     }
+// };
 
 /**
  * Express route handlers
  */
-app.get("/", async (req, res) => {
-    const worker = config.connections.dockerUserDefinedNetwork?.worker;
-    const url = `${worker?.url}:${worker?.port}`;
-    try {
-        const response = await sendGetRequest(url);
-        res.send({
-            data: response,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
+// app.get("/", async (req, res) => {
+//     const worker = config.connections.dockerUserDefinedNetwork?.worker;
+//     const url = `${worker?.url}:${worker?.port}`;
+//     try {
+//         const response = await sendGetRequest(url);
+//         res.send({
+//             data: response,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
-app.get("/api/connect", async (req, res) => {
-    /**
-     * GET /api/connect
-     * server request to worker to request connection to lightning node
-     */
-    try {
-        const axiosResp = await sendGetRequest(workerUrl, "/api/connect");
-        res.send({
-            data: axiosResp,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
+// app.get("/api/connect", async (req, res) => {
+//     /**
+//      * GET /api/connect
+//      * server request to worker to request connection to lightning node
+//      */
+//     try {
+//         const axiosResp = await sendGetRequest(workerUrl, "/api/connect");
+//         res.send({
+//             data: axiosResp,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
-app.get("/api/info", async (req, res) => {
-    /**
-     * GET /api/info
-     * server request to worker to request lightning node info/status
-     */
-    try {
-        const axiosResp = await sendGetRequest(workerUrl, "/api/info");
-        res.send({
-            data: axiosResp,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
+// app.get("/api/info", async (req, res) => {
+//     /**
+//      * GET /api/info
+//      * server request to worker to request lightning node info/status
+//      */
+//     try {
+//         const axiosResp = await sendGetRequest(workerUrl, "/api/info");
+//         res.send({
+//             data: axiosResp,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
-app.get("/api/payment/:id/invoice", async () => {
-    /**
-     * GET /api/payment/:id/invoice
-     * server request to worker to send a payment for a lightning invoice request
-     */
-    try {
-        const axiosResponse = await sendGetRequest(
-            workerUrl,
-            "/api/payment/:id/invoice"
-        );
-        res.send({
-            data: axiosResponse,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
+// app.get("/api/payment/:id/invoice", async () => {
+//     /**
+//      * GET /api/payment/:id/invoice
+//      * server request to worker to send a payment for a lightning invoice request
+//      */
+//     try {
+//         const axiosResponse = await sendGetRequest(
+//             workerUrl,
+//             "/api/payment/:id/invoice"
+//         );
+//         res.send({
+//             data: axiosResponse,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
-app.get("/api/disconnect", async (req, res) => {
-    /**
-     * GET /api/disconnect
-     * server request to worker to request to terminate connection to lightning node
-     */
-    try {
-        const axiosResp = await sendGetRequest(workerUrl, "/api/disconnect");
-        res.send({
-            data: axiosResp,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
+// app.get("/api/disconnect", async (req, res) => {
+//     /**
+//      * GET /api/disconnect
+//      * server request to worker to request to terminate connection to lightning node
+//      */
+//     try {
+//         const axiosResp = await sendGetRequest(workerUrl, "/api/disconnect");
+//         res.send({
+//             data: axiosResp,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
 app.listen(PORT, (err) => {
     console.log(`Server is listening on PORT: ${PORT}`);
--- worker/src/Handler.js
+++ worker/src/Handler.js
@@ -1,3 +1,4 @@
+import config from "../config/config.js";
 import packageJson from "../package.json" assert { type: "json" };
 export default class Handler {
     constructor(app, grpc) {
@@ -7,5 +8,96 @@ export default class Handler {
 
     process() {
         console.log(`Begin ${packageJson.name} process..`);
+        /**
+         * Express route handlers
+         */
+        this.app.get("/", (req, res) => {
+            res.send({
+                message: "Worker service is ready",
+            });
+        });
+
+        this.app.get("/api/connect", async (req, res) => {
+            /**
+             * GET /api/connect
+             */
+            await this.grpc.connect();
+
+            console.log(`grpc.state: ${this.grpc.state}`);
+            res.send({
+                message: `grpc is now connected, state: ${this.grpc.state}`,
+            });
+        });
+
+        this.app.get("/api/info", async (req, res) => {
+            /**
+             * GET /api/info
+             * get the Lightning Node info
+             */
+            console.log(`grpc.state: ${this.grpc.state}`);
+
+            const { Lightning } = this.grpc.services;
+
+            // const balance = await Lightning.walletBalance();
+            const info = await Lightning.getInfo();
+            console.log(JSON.stringify(info, null, 2));
+
+            res.send({
+                message: `Lightning: ${JSON.stringify(info, null, 2)}`,
+            });
+        });
+
+        this.app.get("/api/payment/:id/invoice", async () => {
+            /**
+             * POST /api/payment/:id/invoice
+             * send a payment for a lightning invoice request
+             */
+            console.log(`grpc.state: ${this.grpc.state}`);
+
+            let request = {
+                payment_request: req.body.paymentRequest,
+                timeout_seconds: config.defaultTimeout,
+            };
+
+            await this.grpc.connect();
+
+            let call = await this.grpc.services.Router.sendPaymentV2(request);
+
+            call.on("data", (response) => {
+                console.log(
+                    `Payment response: ${JSON.stringify(response, null, 2)}`
+                );
+                if (response.status.toLowerCase() === "succeeded") {
+                    res.send(response);
+                }
+            });
+
+            call.on("status", (status) => {
+                // The current status of the stream.
+                console.log(`Payment stream status: ${status}`);
+            });
+            call.on("end", async () => {
+                // The server has closed the stream.
+                console.log(
+                    "Payment stream has ended.  It is okay to disconnect."
+                );
+                // Disconnect from all services.
+                // await this.grpc.disconnect();
+            });
+        });
+
+        this.app.get("/api/disconnect", async (req, res) => {
+            /**
+             * GET /api/disconnect
+             * Disconnect from all gRPC services. It's important to disconnect
+             * from the lnd node once you have finished using it. This will free
+             * up any open handles that could prevent your application from
+             * properly closing.
+             */
+            console.log(`grpc.state: ${this.grpc.state}`);
+
+            await this.grpc.disconnect();
+            res.send({ message: "grpc is now disconnected" });
+        });
     }
 }
